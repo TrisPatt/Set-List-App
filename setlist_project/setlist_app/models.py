5d29830d0805ff1131f9acd2b6c4570411d3d5f2
Tristan <tristanpattison@yahoo.co.uk>
2025-03-03 20:10:50 +0000

    save settings

--- setlist_project/setlist_app/models.py
+++ setlist_project/setlist_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    duration = models.FloatField(help_text="Duration in minutes")
+
+    def __str__(self):
+        return f"{self.title} ({self.duration} mins)"
+
+class Setlist(models.Model):
+    SET_CHOICES = [(1, "Set 1"), (2, "Set 2")]
+
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    set_number = models.IntegerField(choices=SET_CHOICES)
+
+    def __str__(self):
+        return f"{self.song.title} - Set {self.set_number}"
+

7c5de4084629e0ccb9a1528e31e0abdc8b4e2c54
Tristan <tristanpattison@yahoo.co.uk>
2025-03-05 17:01:26 +0000

    add set duration to home function in views.py and home.html

--- setlist_project/setlist_app/views.py
+++ setlist_project/setlist_app/views.py
@@ -3,10 +3,18 @@ from .models import Song, Setlist
 from .forms import SongForm
 
 def home(request):
+    # get all songs
     songs = Song.objects.all()
+
+    # get sets
     set1 = Setlist.objects.filter(set_number=1)
     set2 = Setlist.objects.filter(set_number=2)
-    return render(request, 'setlist_app/home.html', {"songs": songs, "set1": set1, "set2": set2})
+
+    # get duration of each set
+    set1_duration = sum(song.song.duration for song in set1)
+    set2_duration = sum(song.song.duration for song in set1)
+    return render(request, 'setlist_app/home.html', {"songs": songs, "set1": set1, "set2": set2, "set1_duration": set1_duration,
+        "set2_duration": set2_duration,})
 
 def add_song(request):
     if request.method == "POST":

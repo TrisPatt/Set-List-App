2bce72bad34943ec43479b75938f12f972ecb885
Tristan <tristanpattison@yahoo.co.uk>
2025-03-07 20:36:56 +0000

    Add unique_together meta to setlist class in models.py

--- setlist_project/setlist_app/models.py
+++ setlist_project/setlist_app/models.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Song(models.Model):
     title = models.CharField(max_length=255, unique=True)
-    duration = models.DurationField(help_text="Duration in MM:SS format")  # Changed to DurationField
+    duration = models.DurationField(help_text="Duration in MM:SS format")  
 
     def __str__(self):
         minutes, seconds = divmod(self.duration.total_seconds(), 60)
@@ -20,6 +20,9 @@ class Setlist(models.Model):
 
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     set_number = models.IntegerField(choices=SET_CHOICES)
+
+    class Meta:
+        unique_together = ('song', 'set_number')
 
     def __str__(self):
         return f"{self.song.title} - Set {self.set_number}"
